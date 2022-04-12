261373140e05b28cfa8216b404b43a071f7666b0
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2022-04-12 02:04:26 -0400

    initial files

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+
+data = {
+    "1":{
+        "sample":"This text is a sample"
+        }
+}
+
+
+@app.route('/')
+def homepage():
+   return render_template('homepage.html', data = data)
+
+
+@app.route('/learn')
+def learn():
+    return render_template('learn.html', data = data)
+    
+
+@app.route('/learn/<id>')
+def view_dessert(id=None):
+    index = id
+    
+    return render_template('view_dessert.html', data = data, index = index)
+    
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+    return render_template('quiz.html')
+    
+    
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
