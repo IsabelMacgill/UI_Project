c06a9131a855a582a744a2c232f365c8c442c75e
Derek Lorenzo <87837145+DerekLorenzo@users.noreply.github.com>
2022-05-05 01:32:38 -0400

    cleaned commented out code

--- static/quiz.js
+++ static/quiz.js
@@ -1,7 +1,5 @@
 var answerResults;
 
-console.log(user["score"])
-
 $(function(){
 	if (question["format"] === "choice"){
 		createChoiceQuestion(desserts, question)
@@ -11,17 +9,19 @@ $(function(){
 });
 
 $(document).ready(function(){
-	console.log(desserts)
-	console.log(question)
-	console.log(user)
 	let clicks = 0
 	let correct = -1
 	
 	if (question["format"] === "choice"){
 		$( "#next" ).click(function(){
 			if(clicks%2 == 0){
+				
+				$('input[name="answers"]').attr('disabled', 'disabled');
+				
 				if ($('input[name="answers"]:checked').length > 0){
+					
 					let selected = $('input[name="answers"]:checked').attr("id").match(/(\d+)/)[0]
+					
 					if ($('input[name="answers"]:checked').val() === question["answer"]){
 						$('label[for='+ selected +']').addClass('correct')
 					} else{
@@ -30,22 +30,24 @@ $(document).ready(function(){
 								correct = i + 1
 							}
 						}
-						console.log(correct)
+						
 						$('label[for='+ selected +']').addClass('incorrect')
 						$('label[for='+ correct +']').addClass('correct')
 					}
+					
 					$('#next').text("Continue")
-					$('input[name=answers]').attr("disabled",true);	
 				} else{
 					for (i=0; i<question["options"].length; i++){
 						if(question["options"][i] === question["answer"]){
 							correct = i + 1
 						}
 					}
+					
 					$('label[for='+ correct +']').addClass('correct')
 				}
 			} else{
 				user["visited"].push(question_value)
+				
 				if ($('input[name="answers"]:checked').val() === question["answer"]){
 					var updated_user = {
 							"id": user["id"],
@@ -61,6 +63,7 @@ $(document).ready(function(){
 							"score": user["score"]				
 					}
 				}
+				
 				if (updated_user["total"] === "10"){
 					$.ajax({
 						type: "POST",
@@ -96,8 +99,8 @@ $(document).ready(function(){
 						}
 					});
 				}
-				
 			}
+			
 			clicks = clicks + 1
 		});
 	} else{
@@ -121,6 +124,7 @@ $(document).ready(function(){
 				$('#next').text("Continue")
 			}else{
 				let curScore = 0.0
+				
 				for (i=0; i<answerResults.length; i++){
 					if (answerResults[i] === true){
 						curScore = curScore + 0.2
@@ -172,7 +176,9 @@ $(document).ready(function(){
 					});
 				}
 			}
+			
 			clicks = clicks + 1
+			
 		});
 	}
 });
@@ -182,11 +188,14 @@ function createChoiceQuestion(desserts, question){
 	let setEntry = document.createElement('div')
 	setEntry.classList.add('row')
 	setEntry.setAttribute("id", "question")
+	
 	let setQuestionEntry = document.createElement('div')
 	setEntry.classList.add('col-12')
+	
 	let setQuestion = document.createElement('div')
 	setQuestion.classList.add('row')
 	setQuestion.classList.add('question')
+	
 	let setQuestionImage = document.createElement('img')
 	setQuestionImage.classList.add('choice-img')
 	setQuestionImage.src = desserts[question["id"]]["image"]
@@ -194,6 +203,7 @@ function createChoiceQuestion(desserts, question){
 	let setAnswerEntry = document.createElement('div')
 	setAnswerEntry.classList.add('row')
 	setAnswerEntry.setAttribute("id", "answers")
+	
 	let answerForm = document.createElement("form")
 
 	let divGroup1 = document.createElement("div")
@@ -204,40 +214,48 @@ function createChoiceQuestion(desserts, question){
 	firstAnswer.setAttribute("id", "option-1")
 	firstAnswer.name = "answers"
 	firstAnswer.value = "1"
+	
 	let firstAnswerLabel = document.createElement("label")
 	firstAnswerLabel.setAttribute("for", "1")
 
 	let divGroup2 = document.createElement("div")
 	divGroup2.setAttribute("class", "group");
+	
 	let secondAnswer = document.createElement("input")
 	secondAnswer.type = "radio"
 	secondAnswer.setAttribute("id", "option-2")
 	secondAnswer.name = "answers"
 	secondAnswer.value = "2"
+	
 	let secondAnswerLabel = document.createElement("label")
 	secondAnswerLabel.setAttribute("for", "2")
 
 	let divGroup3 = document.createElement("div")
 	divGroup3.setAttribute("class", "group");
+	
 	let thirdAnswer = document.createElement("input")
 	thirdAnswer.type = "radio"
 	thirdAnswer.setAttribute("id", "option-3")
 	thirdAnswer.name = "answers"
 	thirdAnswer.value = "3"
+	
 	let thirdAnswerLabel = document.createElement("label")
 	thirdAnswerLabel.setAttribute("for", "3")
 
 	let divGroup4 = document.createElement("div")
 	divGroup4.setAttribute("class", "group");
+	
 	let fourthAnswer = document.createElement("input")
 	fourthAnswer.type = "radio"
 	fourthAnswer.setAttribute("id", "option-4")
 	fourthAnswer.name = "answers"
 	fourthAnswer.value = "4"
+	
 	let fourthAnswerLabel = document.createElement("label")
 	fourthAnswerLabel.setAttribute("for", "4")
 
 	setAnswerEntry.appendChild(answerForm)
+	
 	let setQuestionMap = document.createElement('img')
 	setQuestionMap.classList.add('img-fluid')
 	setQuestionMap.src = desserts[question["id"]]["map"]
@@ -248,7 +266,6 @@ function createChoiceQuestion(desserts, question){
 	setQuestionEntry.appendChild(setQuestionImage)
 	setEntry.appendChild(setQuestionEntry)
 	$("#prompt").append(setEntry)
-	
 	
 	divGroup1.appendChild(firstAnswer);
 	divGroup1.appendChild(firstAnswerLabel)
@@ -266,15 +283,6 @@ function createChoiceQuestion(desserts, question){
 	divGroup4.appendChild(fourthAnswerLabel)
 	answerForm.appendChild(divGroup4);
 
-	// answerForm.appendChild(firstAnswer)
-	// answerForm.appendChild(firstAnswerLabel)
-	// answerForm.appendChild(secondAnswer)
-	// answerForm.appendChild(secondAnswerLabel)
-	// answerForm.appendChild(thirdAnswer)
-	// answerForm.appendChild(thirdAnswerLabel)
-	// answerForm.appendChild(fourthAnswer)
-	// answerForm.appendChild(fourthAnswerLabel)
-	// setAnswerEntry.appendChild(answerForm)
 	$("#prompt").append(setAnswerEntry)
 	
 	$("#title").append(parseInt(user["total"])+1)
@@ -361,7 +369,6 @@ function createDragQuestion(desserts, question){
 		setImageDiv.classList.add('question-img')
 		setImageDiv.setAttribute("id", "question-image"+j)
 		let setQuestionImage = document.createElement('img')
-		// setQuestionImage.classList.add('img-fluid')
 		setQuestionImage.src = desserts[question["options"][j-1]]["image"]
 		setImageDiv.appendChild(setQuestionImage)
 		setImageRow.appendChild(setImageDiv)
@@ -403,7 +410,6 @@ function createDragQuestion(desserts, question){
 				$(ui.draggable).css('position', 'absolute');
 				$(ui.draggable).css('top', $(this).position().top + 40);
 				$(ui.draggable).css('left', $(this).position().left);
-				console.log($(this).position().left)
 				if(ui.draggable.attr('dropped')){
 					prev = ui.draggable.attr('dropped')
 					$('#' + prev).removeClass('drop')
@@ -416,8 +422,6 @@ function createDragQuestion(desserts, question){
 				} else{
 					answerResults[$(this).attr('id')-1] = false
 				}
-				
-				console.log(answerResults)
 				ui.draggable.attr('dropped', $(this).attr('id'))
 			}
 		});
